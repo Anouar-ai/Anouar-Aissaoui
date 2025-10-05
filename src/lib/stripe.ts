8be50bb54f33951552e2a8fe2ae9077f91ca9865
Anouar Aissaoui <anouaraissaoui.pro@gmail.com>
2025-10-05 02:28:25 +0000

    Try fixing this error: `Runtime Error: Error: A "use server" file can on

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,7 +2,7 @@
 
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: '2024-06-20',
 });
 
