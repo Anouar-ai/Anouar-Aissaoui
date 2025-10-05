5cd4ab13bd04ad4eba8c674f928937a8dfa31c86
Anouar Aissaoui <anouaraissaoui.pro@gmail.com>
2025-10-05 04:31:52 +0000

    PLS FIX THIS BUG ; Render blocking requests Est savings of 160 ms Reques

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
       <body
         className={cn(
           'font-body antialiased flex flex-col min-h-screen bg-background',
+          inter.className,
           inter.variable
         )}
       >
